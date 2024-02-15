de28feb73d2dd4151235a2599d267dfd48f70d5c
macronova <trivial@invariantspace.com>
2024-02-14 19:28:59 -0800

    Improve static compile

--- build.rs
+++ build.rs
@@ -6,40 +6,29 @@ fn main() {
     wrapper.file("src/ctranslate2/wrapper.c").std("c++20");
     #[cfg(feature = "static")]
     {
-        let onednn_static = cmake::Config::new(env!("ONEDNN_SRC"))
+        let out_dir =
+            std::env::var("OUT_DIR").expect("Cannot find $OUT_DIR, which should be set by cargo");
+        cmake::Config::new(env!("ONEDNN_SRC"))
             .define("CMAKE_DISABLE_FIND_PACKAGE_GIT", "TRUE")
+            .define("CMAKE_ARCHIVE_OUTPUT_DIRECTORY", out_dir.clone())
             .define("ONEDNN_LIBRARY_TYPE", "STATIC")
             .define("ONEDNN_BUILD_EXAMPLES", "OFF")
             .define("ONEDNN_BUILD_TESTS", "OFF")
             .define("ONEDNN_EXPERIMENTAL", "ON")
             .build();
-        let ctranslate2_static = cmake::Config::new(env!("CTRANSLATE2_SRC"))
+        cmake::Config::new(env!("CTRANSLATE2_SRC"))
             .define("BUILD_CLI", "OFF")
             .define("BUILD_SHARED_LIBS", "OFF")
+            .define("CMAKE_ARCHIVE_OUTPUT_DIRECTORY", out_dir.clone())
             .define("OPENMP_RUNTIME", "COMP")
             .define("WITH_DNNL", "ON")
             .define("WITH_MKL", "OFF")
             .build();
-        std::fs::copy(
-            ctranslate2_static.join("build/third_party/cpu_features/libcpu_features.a"),
-            ctranslate2_static.join("lib64/libcpu_features.a"),
-        )
-        .expect("Unable to find cpu_features static library");
-        println!(
-            "cargo:rustc-link-search=native={}",
-            onednn_static.join("lib64").display()
-        );
-        println!(
-            "cargo:rustc-link-search=native={}",
-            ctranslate2_static.join("lib64").display()
-        );
         println!("cargo:rustc-link-arg=-fopenmp");
         println!("cargo:rustc-link-lib=static=dnnl");
         println!("cargo:rustc-link-lib=static=cpu_features");
         println!("cargo:rustc-link-lib=static=ctranslate2");
-        wrapper
-            .include(ctranslate2_static.join("include"))
-            .include(onednn_static.join("include"));
+        wrapper.include(std::path::Path::new(out_dir.as_str()).join("include"));
     }
     #[cfg(not(feature = "static"))]
     println!("cargo:rustc-link-lib=dylib=ctranslate2");
